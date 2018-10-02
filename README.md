5b8af7642b8504fede4d741aec1466d1ffe4c96e
Michael Melanson <michael@michaelmelanson.net>
2018-10-01 22:10:10 -0400

    Add README

---
+++ README.md
@@ -0,0 +1,39 @@
+# Kernel
+
+This is a place for me to tinker around with kernel development. Feel free to tinker, and let me know if you run into problems.
+
+## Running
+
+### Setup
+
+This requires a bit of a weird Rust development environment. Roughly you'll need:
+
+* A Rust installation installed through `rustup`. This only works on nightly versions since about August 2018.
+* *Mac OS X:* A cross-compiled version of binutils as described here: https://os.phil-opp.com/cross-compile-binutils/
+* A checkout of the Bootloader crate checked out next to this: https://github.com/rust-osdev/bootloader/
+  * You'll need to change the `"linker"` setting to `"x86_64-elf-ld.bfd"` to point to the cross-compiled binutils.
+
+After that, you should be able to run this to get the rest of the dependencies:
+
+```
+make setup
+```
+
+### Running
+
+To run in Qemu:
+
+```
+make run
+```
+
+## Status
+
+[x] It boots!
+[x] Simple VGA driver
+  [x] Rust runtime support for `println!`
+[x] Interrupts
+[ ] Paging (WIP)
+[ ] User mode task switching
+[ ] RTL8139 network driver
+[ ] ...
--- src/arch/x86_64/interrupts/mod.rs
+++ src/arch/x86_64/interrupts/mod.rs
@@ -1,117 +1,99 @@
 use spin::Mutex;
-use x86_64::structures::idt::{ExceptionStackFrame, InterruptDescriptorTable, PageFaultErrorCode};
+use x86_64::structures::idt::{InterruptDescriptorTable, ExceptionStackFrame, PageFaultErrorCode};
 
-static mut INTERRUPTS: Mutex<InterruptDescriptorTable> =
-    Mutex::new(InterruptDescriptorTable::new());
+static mut INTERRUPTS: Mutex<InterruptDescriptorTable> = Mutex::new(InterruptDescriptorTable::new());
 
-extern "x86-interrupt" fn exception_handler(
-    stack_frame: &mut ExceptionStackFrame,
-    error_code: u64,
-) {
-    println!("EXCEPTION:");
-    println!("{:#?}", stack_frame);
-    println!("Error code: {}", error_code);
-    loop {}
+
+extern "x86-interrupt" fn exception_handler(stack_frame: &mut ExceptionStackFrame, error_code: u64)
+{
+  println!("EXCEPTION:");
+  println!("{:#?}", stack_frame);
+  println!("Error code: {}", error_code);
+  loop{}
 }
 
-extern "x86-interrupt" fn trap_handler(stack_frame: &mut ExceptionStackFrame) {
-    println!("INTERRUPT: \n{:#?}", stack_frame);
+extern "x86-interrupt" fn trap_handler(stack_frame: &mut ExceptionStackFrame)
+{
+  println!("INTERRUPT: \n{:#?}", stack_frame);
 }
 
 type InterruptHandler = extern "x86-interrupt" fn(_: &mut ExceptionStackFrame);
-type PageFaultHandler =
-    extern "x86-interrupt" fn(_: &mut ExceptionStackFrame, _: PageFaultErrorCode);
+type PageFaultHandler = extern "x86-interrupt" fn(_: &mut ExceptionStackFrame, _: PageFaultErrorCode);
 type ExceptionHandler = extern "x86-interrupt" fn(_: &mut ExceptionStackFrame, error_code: u64);
 
 pub fn set_page_fault_handler(handler: PageFaultHandler) {
-    unsafe {
-        INTERRUPTS.lock().page_fault.set_handler_fn(handler);
-    }
+  unsafe {
+    INTERRUPTS.lock().page_fault.set_handler_fn(handler);
+  }
 }
 
 pub fn set_general_protection_fault_handler(handler: ExceptionHandler) {
-    unsafe {
-        INTERRUPTS
-            .lock()
-            .general_protection_fault
-            .set_handler_fn(handler);
-    }
+  unsafe {
+    INTERRUPTS.lock().general_protection_fault.set_handler_fn(handler);
+  }
 }
 
 pub fn set_double_fault_handler(handler: ExceptionHandler) {
-    unsafe {
-        INTERRUPTS.lock().double_fault.set_handler_fn(handler);
-    }
+  unsafe {
+    INTERRUPTS.lock().double_fault.set_handler_fn(handler);
+  }
 }
 
 pub fn install() {
-    use core::mem::size_of;
-    use x86_64::instructions::tables::{lidt, DescriptorTablePointer};
+  use x86_64::instructions::tables::{lidt, DescriptorTablePointer};
+  use core::mem::size_of;
 
-    let mut interrupts = unsafe { INTERRUPTS.lock() };
+  let mut interrupts = unsafe { INTERRUPTS.lock() };
 
-    extern "x86-interrupt" fn gpf_handler(stack: &mut ExceptionStackFrame, error_code: u64) {
-        panic!(
-            "General protection fault!\nError code {}, stack: {:?}",
-            error_code, stack
-        );
-    };
+  extern "x86-interrupt" fn gpf_handler(stack: &mut ExceptionStackFrame, error_code: u64) {
+    panic!("General protection fault!\nError code {}, stack: {:?}", error_code, stack);
+  };
 
-    extern "x86-interrupt" fn double_fault_handler(
-        stack: &mut ExceptionStackFrame,
-        error_code: u64,
-    ) {
-        panic!(
-            "Double fault!\nError code {}, stack: {:?}",
-            error_code, stack
-        );
-    };
+  extern "x86-interrupt" fn double_fault_handler(stack: &mut ExceptionStackFrame, error_code: u64) {
+    panic!("Double fault!\nError code {}, stack: {:?}", error_code, stack);
+  };
 
-    interrupts
-        .general_protection_fault
-        .set_handler_fn(gpf_handler);
-    interrupts.double_fault.set_handler_fn(double_fault_handler);
 
-    extern "x86-interrupt" fn irq_handler(_stack: &mut ExceptionStackFrame) {
-        let isr = ::arch::x86_64::pic::get_isr();
 
-        match isr {
-            1 => { /* println!("TICK"); */ }
-            2 => {
-                let scan_code = unsafe { ::x86::io::inb(0x60) };
-                println!("Keyboard scan code {}", scan_code);
-            }
-            irq => {
-                panic!("Unknown IRQ {}", irq);
-            }
-        }
+  interrupts.general_protection_fault.set_handler_fn(gpf_handler);
+  interrupts.double_fault.set_handler_fn(double_fault_handler);
 
-        unsafe {
-            x86::io::outb(0x20, 0x20);
-        }
-    };
+  extern "x86-interrupt" fn irq_handler(_stack: &mut ExceptionStackFrame) {
+    let isr = ::arch::x86_64::pic::get_isr();
 
-    for i in 0x20..0x2F {
-        interrupts[i].set_handler_fn(irq_handler);
+    match isr {
+      1 => { /* println!("TICK"); */ },
+      2 => { 
+        let scan_code = unsafe { ::x86::io::inb(0x60) };
+        println!("Keyboard scan code {}", scan_code); 
+      },
+      irq => { panic!("Unknown IRQ {}", irq); }
     }
 
     unsafe {
-        let idt_base = &*interrupts as *const _ as u64;
-        let idt_limit = (size_of::<InterruptDescriptorTable>() - 1) as u16;
-
-        let table = DescriptorTablePointer {
-            base: idt_base,
-            limit: idt_limit,
-        };
-
-        println!(
-            "Installing IDT: base={:016x?}, limit={:08x?}",
-            idt_base, idt_limit
-        );
-
-        lidt(&table);
-        x86::irq::enable();
+      x86::io::outb(0x20, 0x20);
     }
+  };
 
-    println!("We have interrupts!");
+  for i in 0x20..0x2F {
+    interrupts[i].set_handler_fn(irq_handler);
+  }
+
+
+  unsafe {
+    let idt_base = &*interrupts as *const _ as u64;
+    let idt_limit = (size_of::<InterruptDescriptorTable>() - 1) as u16;
+
+    let table = DescriptorTablePointer {
+      base: idt_base,
+      limit: idt_limit
+    };
+
+    println!("Installing IDT: base={:016x?}, limit={:08x?}", idt_base, idt_limit);
+
+    lidt(&table);
+    x86::irq::enable();
+  }
+
+  println!("We have interrupts!");
 }
